383503e9491df054116656aef91bee1a05335ab8
M A S T E R <kiril.svetoslavov.kirilov@gmail.com>
2022-11-16 23:50:55 +0200

    Initial Commit!

---
+++ src/main/java/ConsoleRunner.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.CommandLineRunner;
+
+public class ConsoleRunner implements CommandLineRunner {
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+    }
+}
